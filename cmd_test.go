a330966955abf54b3b6da1ad515ae0a05e5d4c7c
Eyal Posener <posener@gmail.com>
2019-11-27 22:44:27 +0200

    Move complete tests

--- cmd_test.go
+++ cmd_test.go
@@ -7,7 +7,6 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/posener/complete/v2"
 	"github.com/posener/complete/v2/predict"
 	"github.com/stretchr/testify/assert"
 )
@@ -394,30 +393,4 @@ func TestCmd_failures(t *testing.T) {
 
 		assert.Panics(t, func() { root.Args("", "") })
 	})
-}
-
-func TestComplete(t *testing.T) {
-	t.Parallel()
-
-	comp := (*completer)(newTestCmd().SubCmd)
-
-	tests := []struct {
-		line        string
-		completions []string
-	}{
-		// Check completion of sub commands.
-		{line: "su", completions: []string{"sub1", "sub2"}},
-		// Check completion of flag names.
-		{line: "sub1 sub1 -f", completions: []string{"-flag1", "-flag0", "-flag11"}},
-		// Check completion of flag values.
-		{line: "sub1 sub1 -flag1 ", completions: []string{"foo", "bar"}},
-		// Check completion for positional arguments.
-		{line: "sub2 ", completions: []string{"-flag0", "-h", "one", "two"}},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.line, func(t *testing.T) {
-			complete.Test(t, comp, tt.line, tt.completions)
-		})
-	}
 }
